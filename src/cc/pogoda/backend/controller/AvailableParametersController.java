200e9ba5f18f1634538eafc8c0f65eed5cccddda
Mateusz Lubecki <sp8ebc@interia.pl>
2021-12-08 00:56:52 +0100

    endpoint to return available parameters for given station

---
+++ src/cc/pogoda/backend/controller/AvailableParametersController.java
@@ -0,0 +1,34 @@
+package cc.pogoda.backend.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import cc.pogoda.backend.dao.StationsDefinitionDao;
+import cc.pogoda.backend.types.model.AvailableParameters;
+import cc.pogoda.backend.types.model.StationDefinition;
+
+@RestController
+public class AvailableParametersController {
+
+	@Autowired
+	StationsDefinitionDao definitionDao;
+	
+	@RequestMapping(value = "/station/{stationName}/availableParameters", produces = "application/json;charset=UTF-8")
+	public AvailableParameters getAvailableParametersForStation(@PathVariable(required = true)String stationName) {
+		
+		AvailableParameters out = new AvailableParameters();
+		
+		StationDefinition station_def = definitionDao.getStationByName(stationName);
+		
+		out.hasHumidity = station_def.hasHumidity;
+		out.hasQnh = station_def.hasQnh;
+		out.hasRain = station_def.hasRain;
+		out.hasWind = station_def.hasWind;
+		out.telemetryVersion = station_def.telemetryVersion;
+		
+		return out;
+	}
+ 
+}
---
+++ src/cc/pogoda/backend/types/model/AvailableParameters.java
@@ -0,0 +1,15 @@
+package cc.pogoda.backend.types.model;
+
+public class AvailableParameters {
+
+	
+	public boolean hasWind;
+	
+	public boolean hasQnh;
+	
+	public boolean hasHumidity;
+	
+	public boolean hasRain;
+	
+	public byte telemetryVersion;
+}
