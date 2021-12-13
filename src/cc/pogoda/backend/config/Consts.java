4dc7c498fa899d53ba2a10aed83aed1cbafb434f
Mateusz Lubecki <sp8ebc@interia.pl>
2021-12-13 12:21:25 +0100

    version bumbed

--- src/cc/pogoda/backend/config/Consts.java
+++ src/cc/pogoda/backend/config/Consts.java
@@ -6,7 +6,7 @@ public class Consts {
 	
 	public static final int PATCH_VERSION_NUM = 0;
 	
-	public static final int BUILD_VERSION_NUM = 19;
+	public static final int BUILD_VERSION_NUM = 20;
 	
 	public static final String VERSION = "meteo_backend_" + MAIN_VERSION_NUM + "-" + PATCH_VERSION_NUM + "-" + BUILD_VERSION_NUM;
 }
