bac66772835bea96613d25d69ddf3629daa9f73d
Mateusz Lubecki <sp8ebc@interia.pl>
2024-02-05 18:12:32 +0100

    removed obsolete endpoint

--- src/cc/pogoda/backend/controller/SummaryController.java
+++ src/cc/pogoda/backend/controller/SummaryController.java
@@ -43,7 +43,6 @@ public class SummaryController {
 	
 	@RequestMapping(value = "/station/{stationName}/summary", produces = "application/json;charset=UTF-8")
 	public Summary summaryControlerNew(@PathVariable(required = true)String stationName) throws NotFoundException {
-		//Summary s = dao.getSummaryPerStationName(stationName);
 		Summary s = dao.getNewSummaryPerStationName(stationName);
 		
 		long currentUtcTimestamp = ZonedDateTime.now().withZoneSameInstant(ZoneId.of("UTC")).toEpochSecond();
@@ -106,69 +105,69 @@ public class SummaryController {
 		
 	}
 	
-	@Deprecated
-	@RequestMapping(value = "/summary", produces = "application/json;charset=UTF-8")
-	public Summary summaryControlerOld(@RequestParam(value="station")String station) throws NotFoundException {
-		Summary s = dao.getSummaryPerStationName(station);
-	
-		logger.info("[SummaryController][summaryControlerOld][request.getRemoteAddr() = " + request.getRemoteAddr() + "][station = " + station +"][s = " + s.toString() +"]");
-		
-		long currentUtcTimestamp = ZonedDateTime.now().withZoneSameInstant(ZoneId.of("UTC")).toEpochSecond();
-		
-		WindQualityFactor wind_qf;
-		TemperatureQualityFactor temperature_qf;
-		PressureQualityFactor pressure_qf;
-		HumidityQualityFactor humidity_qf;
-		
-		if (s != null) {
-			
-			StationDefinitionModel stationDefinition = stationsDefinitionDao.getStationByName(station);
-			
-			TelemetryModel t = telemetry.getTelemetryFromStationName(station);
-			
-			if (t != null && stationDefinition.telemetryVersion != 0) {
-				wind_qf =  WindQualityFactor.fromBits((byte) t.digital, stationDefinition.telemetryVersion);
-				temperature_qf = TemperatureQualityFactor.fromBits((byte) t.digital, stationDefinition.telemetryVersion);
-				pressure_qf = PressureQualityFactor.fromBits((byte) t.digital, stationDefinition.telemetryVersion);
-				humidity_qf = HumidityQualityFactor.fromBits((byte) t.digital, stationDefinition.telemetryVersion);
-			}
-			else {
-				wind_qf = WindQualityFactor.FULL;
-				temperature_qf = TemperatureQualityFactor.FULL;
-				pressure_qf = PressureQualityFactor.FULL;
-				humidity_qf = HumidityQualityFactor.FULL;
-			}
-			
-			if (!stationDefinition.hasWind) {
-				wind_qf = WindQualityFactor.NOT_AVALIABLE;
-			}
-			
-			if (!stationDefinition.hasQnh) {
-				pressure_qf = PressureQualityFactor.NOT_AVALIABLE;
-			}
-			
-			if (!stationDefinition.hasHumidity) {
-				humidity_qf = HumidityQualityFactor.NOT_AVALIABLE;
-			}
-			
-			if ((currentUtcTimestamp - s.last_timestamp > 3600)) {
-				wind_qf = WindQualityFactor.NO_DATA;
-				pressure_qf = PressureQualityFactor.NO_DATA;
-				humidity_qf = HumidityQualityFactor.NO_DATA;
-			}
-			
-			s.qnh_qf = pressure_qf.toString();
-			s.wind_qf = wind_qf.toString();
-			s.temperature_qf = temperature_qf.toString();
-			s.humidity_qf = humidity_qf.toString();
-			
-			return s;
-		}
-		else {
-			throw new NotFoundException();
-		}
-		
-	}
+//	@Deprecated
+//	@RequestMapping(value = "/summary", produces = "application/json;charset=UTF-8")
+//	public Summary summaryControlerOld(@RequestParam(value="station")String station) throws NotFoundException {
+//		Summary s = dao.getSummaryPerStationName(station);
+//	
+//		logger.info("[SummaryController][summaryControlerOld][request.getRemoteAddr() = " + request.getRemoteAddr() + "][station = " + station +"][s = " + s.toString() +"]");
+//		
+//		long currentUtcTimestamp = ZonedDateTime.now().withZoneSameInstant(ZoneId.of("UTC")).toEpochSecond();
+//		
+//		WindQualityFactor wind_qf;
+//		TemperatureQualityFactor temperature_qf;
+//		PressureQualityFactor pressure_qf;
+//		HumidityQualityFactor humidity_qf;
+//		
+//		if (s != null) {
+//			
+//			StationDefinitionModel stationDefinition = stationsDefinitionDao.getStationByName(station);
+//			
+//			TelemetryModel t = telemetry.getTelemetryFromStationName(station);
+//			
+//			if (t != null && stationDefinition.telemetryVersion != 0) {
+//				wind_qf =  WindQualityFactor.fromBits((byte) t.digital, stationDefinition.telemetryVersion);
+//				temperature_qf = TemperatureQualityFactor.fromBits((byte) t.digital, stationDefinition.telemetryVersion);
+//				pressure_qf = PressureQualityFactor.fromBits((byte) t.digital, stationDefinition.telemetryVersion);
+//				humidity_qf = HumidityQualityFactor.fromBits((byte) t.digital, stationDefinition.telemetryVersion);
+//			}
+//			else {
+//				wind_qf = WindQualityFactor.FULL;
+//				temperature_qf = TemperatureQualityFactor.FULL;
+//				pressure_qf = PressureQualityFactor.FULL;
+//				humidity_qf = HumidityQualityFactor.FULL;
+//			}
+//			
+//			if (!stationDefinition.hasWind) {
+//				wind_qf = WindQualityFactor.NOT_AVALIABLE;
+//			}
+//			
+//			if (!stationDefinition.hasQnh) {
+//				pressure_qf = PressureQualityFactor.NOT_AVALIABLE;
+//			}
+//			
+//			if (!stationDefinition.hasHumidity) {
+//				humidity_qf = HumidityQualityFactor.NOT_AVALIABLE;
+//			}
+//			
+//			if ((currentUtcTimestamp - s.last_timestamp > 3600)) {
+//				wind_qf = WindQualityFactor.NO_DATA;
+//				pressure_qf = PressureQualityFactor.NO_DATA;
+//				humidity_qf = HumidityQualityFactor.NO_DATA;
+//			}
+//			
+//			s.qnh_qf = pressure_qf.toString();
+//			s.wind_qf = wind_qf.toString();
+//			s.temperature_qf = temperature_qf.toString();
+//			s.humidity_qf = humidity_qf.toString();
+//			
+//			return s;
+//		}
+//		else {
+//			throw new NotFoundException();
+//		}
+//		
+//	}
 	
 	
 }
